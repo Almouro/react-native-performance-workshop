934347053891d4c3d709b98da5379b88a3680a03
almouro <alex@moureaux.me>
2024-11-14 11:56:14 +0000

    wip

--- src/data/formatPhotos.ts
+++ src/data/formatPhotos.ts
@@ -1,9 +1,11 @@
 import { Photo } from "./model/photo.quicktype";
 
+const formatter = new Intl.DateTimeFormat("en-US", {
+  month: "2-digit",
+  day: "2-digit",
+});
+
 export const formatPhoto = (photo: Photo) => ({
   ...photo,
-  pub: new Intl.DateTimeFormat("en-US", {
-    month: "2-digit",
-    day: "2-digit",
-  }).format(new Date(photo.createdAt)),
+  pub: formatter.format(new Date(photo.createdAt)),
 });
